0df7afa69c6047685b3f07c4fd0564634cf3e477
udasrivarshini <udasrivarshini@gmail.com>
2023-04-12 10:25:23 +0530

    Initial Commit

---
+++ backend/database/dbconnection.js
@@ -0,0 +1,17 @@
+/*const mongoose = require('mongoose');
+
+
+mongoose.connect("mongodb://localhost:27017/", {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false
+}).then(() => {
+
+    console.log('connection successful');
+
+}).catch((err) => {
+
+    console.log('no connection');
+
+});*/
---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React, { createContext, useReducer } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./components/Home"
+import { initialState, reducer } from "../src/reducer/UseReduser"
+import Signup from "./components/Signup";
+
+export const UserContext = createContext();
+
+function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return ( <
+        BrowserRouter >
+        <
+        UserContext.Provider value = {
+            { state, dispatch }
+        } >
+        <
+        Routes >
+        <
+        Route exact path = "/"
+        element = { < Home / > }
+        /> <
+        Route exact path = "/login"
+        element = { < Signup / > }
+        /> < /
+        Routes > <
+        /UserContext.Provider> < /
+        BrowserRouter >
+    );
+}
+
+export default App;
