2a5e965c04aa9b699fbbd44fc7c451e83b0b58eb
udasrivarshini <udasrivarshini@gmail.com>
2023-04-29 16:10:43 +0530

    CSS changes and edits

--- backend/controllers/messages.js
+++ backend/controllers/messages.js
@@ -5,57 +5,57 @@ const userAuth = require("../middelware/userAuth");
 
 const User = require('../models/userSchema');
 const Message = require('../models/message');
-const Chat = require('../models/chatSchema'); 
+const Chat = require('../models/chatSchema');
 
 
-module.exports = router.post('/getAllMsgs', userAuth, async(req, res)=>{
+module.exports = router.post('/getAllMsgs', userAuth, async(req, res) => {
     const selectedId = req.body.selectedId;
-    const getChat = await Chat.findOne({ users: {$all: [selectedId, req.userID] } });
+    const getChat = await Chat.findOne({ users: { $all: [selectedId, req.userID] } });
     const getMessages = await Message.find({ ChatId: getChat._id });
 
     try {
- 
-        res.status(201).send({getAllMessages: getMessages, chatExist: getChat});
+
+        res.status(201).send({ getAllMessages: getMessages, chatExist: getChat });
 
     } catch (error) {
         res.status(500).send(error.message);
         console.log(error)
-    } 
+    }
 });
 
 
 
-module.exports = router.post('/getAllGroupMsgs', userAuth, async(req, res)=>{
+module.exports = router.post('/getAllGroupMsgs', userAuth, async(req, res) => {
     const selectedId = req.body.selectedId;
     const getChat = await Chat.findOne({ _id: selectedId });
     const getMessages = await Message.find({ ChatId: getChat._id });
 
     try {
- 
-        res.status(201).send({getAllMessages: getMessages, chatExist: getChat});
+
+        res.status(201).send({ getAllMessages: getMessages, chatExist: getChat });
 
     } catch (error) {
         res.status(500).send(error.message);
         console.log(error)
-    } 
+    }
 });
 
 
 
-module.exports = router.post('/sendingGroupMsg', userAuth, async(req, res)=>{
-    const {selectedId, txtInput} = req.body;
+module.exports = router.post('/sendingGroupMsg', userAuth, async(req, res) => {
+    const { selectedId, txtInput } = req.body;
     console.log(selectedId)
-    const getChat = await Chat.findOne({ _id: selectedId});
-    try {    
-        const newMessage = new Message({   
+    const getChat = await Chat.findOne({ _id: selectedId });
+    try {
+        const newMessage = new Message({
             sender: req.userID,
             ChatId: getChat._id,
             message: txtInput,
         });
 
         await newMessage.save();
-       
-        res.status(201).send({newMessage, getChat});
+
+        res.status(201).send({ newMessage, getChat });
     } catch (error) {
         res.status(500).send(error.message);
         console.log(error)
@@ -63,29 +63,27 @@ module.exports = router.post('/sendingGroupMsg', userAuth, async(req, res)=>{
 });
 
 
-module.exports = router.post('/sendingMsg', userAuth, async(req, res)=>{
-    const {selectedId, txtInput} = req.body;
-    const getChat = await Chat.findOne({ users:{ $all: [selectedId, req.userID] }});
-    try {    
-        const newMessage = new Message({   
+module.exports = router.post('/sendingMsg', userAuth, async(req, res) => {
+    const { selectedId, txtInput } = req.body;
+    const getChat = await Chat.findOne({ users: { $all: [selectedId, req.userID] } });
+    try {
+        const newMessage = new Message({
             sender: req.userID,
             ChatId: getChat._id,
             message: txtInput,
         });
 
         await newMessage.save();
-       
-        res.status(201).send({newMessage, getChat});
+
+        res.status(201).send({ newMessage, getChat });
     } catch (error) {
         res.status(500).send(error.message);
         console.log(error)
     }
 });
-
-
-module.exports = router.get('/allGroupChats', userAuth, async(req, res)=>{
+module.exports = router.get('/allGroupChats', userAuth, async(req, res) => {
     // const findChats = await Chat.find({ users : req.userID , isGroupChat: true});
-    const findChats = await Chat.find({ users:{ $all: [req.userID]} , isGroupChat: true});
+    const findChats = await Chat.find({ users: { $all: [req.userID] }, isGroupChat: true });
     try {
 
         res.status(201).send(findChats);
@@ -94,4 +92,3 @@ module.exports = router.get('/allGroupChats', userAuth, async(req, res)=>{
         console.log(error)
     }
 });
-
